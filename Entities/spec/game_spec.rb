dbdd59714a164214f73ba3b51eba0588ce64581e
mathinvalidnik <kazmanov@uni-sofia.bg>
2014-02-12 02:41:12 +0200

    New specs added
    I have added some more spec cases for the Game class methods

--- Entities/spec/game_spec.rb
+++ Entities/spec/game_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'yaml'
 
 describe Game do
   before :each do
@@ -18,15 +19,37 @@ describe Game do
   
   describe "#load_game" do
     it "takes one parameter and loads the saved file" do
+	  objects = [@first_player, @second_player, @source].to_yaml
+
+	  input = YAML.load(objects)
 	  
+	  @first_player.name.should eql input[0].name
+	  @first_player.hand.cards.should eql input[0].hand.cards
+	  
+	  @second_player.name.should eql input[1].name
+	  @second_player.hand.cards.should eql input[1].hand.cards
+	  
+	  @source.source_cards.should eql input[2].source_cards
 	end
   end
   
   describe "#save_game" do
     it "takes one parameter and saves the data into file" do
-	  
+	  #throw new NotImplementedException
 	end
   end
   
-  
+  describe "#deal_cards" do
+    it "takes no parameters and deal seven cards to each player and the others go in the lake" do
+	  @first_player.hand.cards.size.should eql 0
+	  @second_player.hand.cards.size.should eql 0
+	  @source.size_of_source.should eql 0
+	  
+	  @game.deal_cards
+	  
+	  @first_player.hand.cards.size.should eql 7
+	  @second_player.hand.cards.size.should eql 7
+	  @game.lake_as_source.source_cards.size.should eql 38
+	end
+  end
 end
